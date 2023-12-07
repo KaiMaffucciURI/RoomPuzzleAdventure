c014df319abf2db39a45500c8046d716d2b6e5a2
KaiMaffucciURI <123519150+KaiMaffucciURI@users.noreply.github.com>
2023-12-07 14:16:18 -0500

    Finished up backbone of game for most part

--- final.pl
+++ final.pl
@@ -1,5 +1,4 @@
-% TODO: add predicate which prints message if an attempted user apply doesnt work
-
+% text adventure puzzle game
 
 :- dynamic alive/1.
 
@@ -7,20 +6,6 @@
 :- dynamic inv/1.
 % keeps track of what is in the room
 :- dynamic room/1.
-
-% sets up the starting scenario
-start_inv :-
-  assert(inv(hand)),
-  assert(inv(foot)),
-  assert(inv(head)).
-
-start_room :-
-  assert(room(door)),
-  assert(room(bookshelf)),
-  assert(room(chest)),
-  assert(room(painting)),
-  assert(room(nightstand)),
-  assert(room(lamp)).
 
 % predicates to write to screen
 print_inv :- writeln('Items in your inventory: '), forall(inv(X), writeln(X)).
@@ -30,23 +15,64 @@ print_all :- print_inv, writeln(''), print_room.
 % retracts everything from everywhere
 nuke :- retractall(inv(_)), retractall(room(_)). 
 
-startup :-
-  nuke, 
-  start_inv,
-  start_room,
-  writeln('Welcome! You are trapped in a room.\nPick an item in the room to interact with,\nthen another to apply to that item.'),
-  interact.
+% message if player tried to apply something in inv to something in room that doesnt work
+invalid_sel :- writeln('That does not work!').
+
+
+% logic rundown if player selected door as room item
+try_door(Sel) :- (
+  Sel = 'hand' ->
+    writeln('wake open doors!') ;
+  writeln('no waking doors.')
+  ).
+
 
 % main game loop
 % add option so when they type [clear] it clears the screen
 interact :-
   print_all,
   writeln('Choose something in the room to interact with.'),
-  read(Input),
-  ((Input = 'quit' ; Input = 'exit') ->
-    writeln('BYEBYE!'), abort ;
-  (Input = 'balls' ->
-    writeln('hehe!'), abort) ;
-    writeln('hooray!')
+  read(RoomSel),
+  write('You chose '), write(RoomSel), writeln(''),
+  (room(RoomSel) ->
+    (
+      (RoomSel = 'quit' ; RoomSel = 'exit') ->
+        writeln('BYEBYE!'), abort ;
+      RoomSel = 'clear' ->
+        tty_clear ;
+      (
+        writeln('Choose something in your inventory to apply: '),
+        read(InvSel),
+        write('You chose '), write(InvSel), writeln(''),
+        % this is where the magic really happens TODO: check if inv selection was valid with inv(InvSel)
+        (
+          RoomSel = 'door' -> try_door(InvSel) ;
+          invalid_sel
+        )
+      )
+    ) ;
+    writeln('That item is not in the room, please try again.')
   ),
+  interact.
+
+
+% sets up the starting scenario
+start_room :-
+  assert(room(door)),
+  assert(room(bookshelf)),
+  assert(room(chest)),
+  assert(room(painting)),
+  assert(room(nightstand)),
+  assert(room(lamp)).
+
+start_inv :-
+  assert(inv(hand)),
+  assert(inv(foot)),
+  assert(inv(head)).
+
+startup :-
+  nuke, 
+  start_room,
+  start_inv,
+  writeln('Welcome! You are trapped in a room.\nPick an item in the room to interact with,\nthen one in your inventory to apply to that item.'), writeln(''),
   interact.
